f475906fa7d1062fa97c5e9f2fc03e32398cce01
zoltanmajor-seon <zoltan.major@seon.io>
2025-12-06 13:46:09 +0100

    Add template for solving daily puzzles

---
+++ day_template/task_1.py
@@ -0,0 +1,13 @@
+def load_input():
+    with open("input.txt", "r") as f:
+        lines = f.read().split("\n\n")
+
+    return lines
+
+
+def main():
+    data = load_input()
+    print(data)
+
+if __name__ == "__main__":
+    main()
